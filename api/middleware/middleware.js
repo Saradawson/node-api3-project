37b472b3d5da35d26d843c48c5d4686b1f8eed29
unknown <saradawsone@gmail.com>
2022-10-17 12:13:07 -0600

    console logging correct logger response

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,7 +1,9 @@
 function logger(req, res, next) {
   // DO YOUR MAGIC
-
-  console.log('logger middleware')
+  const timestamp = new Date().toLocaleString();
+  const method = req.method;
+  const url = req.originalUrl;
+  console.log(`[${timestamp}] ${method} to ${url}`);
   next();
 }
 
