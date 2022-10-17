e2267cbf4d0a57912a12b48e92c3205fe6749cbd
unknown <saradawsone@gmail.com>
2022-10-17 12:35:44 -0600

    router is connected

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,3 +1,5 @@
+const User = require('../users/users-model');
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
   const timestamp = new Date().toLocaleString();
@@ -7,9 +9,24 @@ function logger(req, res, next) {
   next();
 }
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
   // DO YOUR MAGIC
-
+  try{
+    const user = await User.getById(req.params.id)
+    if(!user){
+      res.status(404).json({
+        message: "user not found"
+      })
+    }else{
+      req.user = user;
+      next();
+    }
+  }catch(err) {
+    res.status(500).json({
+      message: "unable to find user"
+    })
+  }
+  
 }
 
 function validateUser(req, res, next) {
@@ -26,4 +43,5 @@ function validatePost(req, res, next) {
 
 module.exports = {
   logger,
+  validateUserId,
 }
--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const server = express();
+const usersRouter = require('./users/users-router');
 
 // remember express by default cannot parse JSON in request bodies
 server.use(express.json());
@@ -8,6 +9,7 @@ server.use(express.json());
 const { logger } = require('./middleware/middleware')
 
 server.use(logger);
+server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
