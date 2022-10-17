67fb11b702c311c64bfe7e72fda460e54b966c65
unknown <saradawsone@gmail.com>
2022-10-17 14:25:44 -0600

    post request is working with tests but not postman

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -23,20 +23,28 @@ router.get('/', (req, res, next) => {
 router.get('/:id', validateUserId, (req, res) => {
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
-  console.log(req.user);
+  res.json(req.user);
 });
 
-router.post('/', validateUser, (req, res) => {
+router.post('/', validateUser, (req, res, next) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
-  console.log(req.body);
+  User.insert({name: req.name})
+      .then(newUser => {
+        res.status(201).json(newUser)
+      })
+      .catch(next)
 });
 
-router.put('/:id', validateUserId, validateUser, (req, res) => {
+router.put('/:id', validateUserId, validateUser, (req, res, next) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
-  console.log(req.body);
+  User.update(req.params.id, req.body)
+      .then(updatedUser => {
+        console.log(updatedUser)
+      })
+      .catch(next)
 });
 
 router.delete('/:id', validateUserId, (req, res) => {
